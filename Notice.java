d0ac04e050652a3b29d6f1a199c0403340876310
ashanq <79820774+ashanq@users.noreply.github.com>
2022-10-22 21:43:47 +0530

    Create Notice.java
    Adding notice code

---
+++ Notice.java
@@ -0,0 +1,47 @@
+package com.notice;
+
+
+
+public class Notice {
+    
+     private int noticeid;
+        private String title;
+        private String des
+        private String phone;
+        private String email
+        private String date;
+        
+        
+        public Notice(int noticeid, String title, String des,
+                String phone,String email,String date) {
+            this.noticeid = noticeid;
+            this.title = title
+            th.des = des
+            this.phone = phone;
+            this.email = email
+            this.date = date;
+            
+        }
+
+
+
+
+        public int getNoticeid() {
+            return noticeid;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public tring getDes() {
+            return des;
+        }
+        public String getPhone() {
+            return phone
+        }
+        public String getEmail() {
+            return email;
+        }
+        public String getDate() {
+            return date;
+        }
+}
